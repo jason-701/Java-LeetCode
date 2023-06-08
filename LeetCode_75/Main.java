643d669310c7865a78edea574624e5dde77cbb24
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-08 14:37:46 +0800

    product of array except self

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
         array_string foo = new array_string();
-        foo.reverseWords("A Hello     World");
-        System.out.println(foo.reverseWords("A Hello     World"));
+        int[] neee = {-1,1,0,-3,3};
+        System.out.println(foo.productExceptSelf(neee));
     }
 }
--- LeetCode_75/array_string.java
+++ LeetCode_75/array_string.java
@@ -178,4 +178,29 @@ public class array_string {
         String result = builder.toString();
         return result.trim();
     }
+
+    public int[] productExceptSelf(int[] nums) {
+        int[] left = new int[nums.length];
+        int[] right = new int[nums.length];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i]=1;
+            right[i]=1;
+        }
+
+        for (int i = 1; i < left.length; i++) {
+            left[i]=left[i-1]*nums[i-1];
+        }
+        
+        for (int i = right.length-2; i >=0; i--) {
+            right[i]=right[i+1]*nums[i+1];
+        }
+
+
+        int[]result = new int[nums.length];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = left[i]*right[i];
+        }
+        return result;
+    }
 }
