32b456b0785217309ac67d4ccf5344bcc928d72f
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-09 10:32:17 +0800

    move zeroes

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,11 +1,10 @@
 public class Main{
     public static void main(String[] args) {
         array_string foo = new array_string();
-        int[] test = {1,0,0,0,0,0,0,0,1000000000};
-        char[] teeee = {'a','b','c','c','c','c'};
-        char[] beeee = {'a'};
-        System.out.println(foo.compress(teeee));
-        for (char c : teeee) {
+        two_pointers too = new two_pointers();
+        int[] test = {1,2,0,0,44,244,0,0,12,3,5,5,1};
+        too.moveZeroes(test);
+        for (int c : test) {
             System.out.println(c);
         }
     }
