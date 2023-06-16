c9d52f02427b7634473ca5b141f666ea12b58700
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-16 10:53:45 +0800

    find pivot index

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        int[] test_array = {-4,-3,-2,-1,4,3,2};
+        int[] test_array = {1,7,3,6,5,6};
         prefix_sum test = new prefix_sum();
-        System.out.println(test.largestAltitude(test_array));  
+        System.out.println(test.pivotIndex(test_array));  
     }
 }
--- LeetCode_75/prefix_sum.java
+++ LeetCode_75/prefix_sum.java
@@ -7,11 +7,35 @@ public class prefix_sum {
         int max, sum;
         max = sum = 0;
 
-    //  Iterate through the array while getting the cumulative sum and finding the max
+            //  Iterate through the array while getting the cumulative sum and finding the max
         for (int i = 0; i < gain.length; i++) {
             sum += gain[i];
             max = Math.max(max, sum);
         }
         return max;
+    }
+
+    //  Find the pivot index where sum of all elements before the index = sum of all elements after the index
+    public int pivotIndex(int[] nums) {
+        int length = nums.length;
+        int[] left = new int[length];
+        int[] right = new int[length];
+        left[0] = 0;
+        right[right.length-1] = 0;
+
+        //  Left array = sum of all elements to the left of index
+        //  Right array = sum of all elements to the right of index
+        for (int i = 1; i < length; i++) {
+            left[i] = left[i-1] + nums[i-1];
+            right[length-1-i] = right[length-i] + nums[length-i];
+        }
+
+        //  If left[i] = right[i], then it is the pivot index
+        for (int i = 0; i < length; i++) {
+            if (left[i] == right[i]){
+                return i;
+            }
+        }
+        return -1;
     }
 }
