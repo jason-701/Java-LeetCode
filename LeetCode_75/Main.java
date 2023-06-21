82d1b31be39c30999c66e5e29fca03156d3dd51b
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-21 18:04:48 +0800

    small change

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String[] args) {
-        int[][] test_array = {{3,1,2,2},{1,4,4,5},{2,4,2,2},{2,4,2,2}};
+        int[] test_array = {1,-1,-2,-2};
         stack test = new stack();
-        System.out.println(test.removeStars("leet**cod*e"));  
+        test.asteroidCollision(test_array);
     }
 }
