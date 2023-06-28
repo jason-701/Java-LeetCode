0f34ef5fe25638c6c2b227ef75662a2f38b679a4
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-28 22:44:38 +0800

    path with max probability (this ain't no medium)

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,8 +1,9 @@
 public class Main{
     public static void main(String[] args) {
-        int[] nums1 = {1,7,11};
+        int[][] nums1 = {{1,4},{2,4},{0,4},{0,3},{0,2},{2,3}};
+        double[] succProb = {0.37,0.17,0.93,0.23,0.39,0.04};
         int[] nums2 = {2,4,6};
         daily_challenge test = new daily_challenge();
-        System.out.println(test.kSmallestPairs(nums1,nums2, 3));
+        System.out.println(test.maxProbability(5,nums1,succProb, 3,4));
     }
 }
--- LeetCode_75/daily_challenge.java
+++ LeetCode_75/daily_challenge.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class daily_challenge{
 
@@ -59,6 +56,88 @@ public class daily_challenge{
 
         public int getSum(){
             return this.a + this.b;
+        }
+    }
+
+    //  Find the path with the maximum probability
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start, int end) {
+        //  The idea is to use Dijkstra's Algorithm that sorts with the highest probability first
+         
+        //  Priority queue to store node and weight data
+        PriorityQueue<Edge> queue = new PriorityQueue<>(Comparator.comparingDouble(Edge::getWeight).reversed());
+
+        //  Initialize necessary arrays for Djikstra's
+        double[] d = new double[n];
+        int[] s = new int[n];
+        for (int i = 0; i < n; i++) {
+            d[i] = 0.0; // distance array is set to 0 because we want to get the maximum and not the minimum
+            s[i] = 0;            
+        }
+
+        //  Store all adjacent edges of each node
+        Map<Integer, List<Edge>> adjacentEdges = new HashMap<>();
+        for (int i = 0; i < edges.length; i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adjacentEdges.computeIfAbsent(u, ArrayList::new).add(new Edge(u, v, succProb[i]));
+            adjacentEdges.computeIfAbsent(v, ArrayList::new).add(new Edge(v, u, succProb[i]));
+        }
+
+        queue.add(new Edge(start, start, 1));
+        d[start] = 1;
+
+        while (!queue.isEmpty()){
+            Edge temp = queue.poll();
+            s[temp.getNode2()] = 1;
+            if (temp.getNode2() == end){
+                return temp.getWeight();
+            }
+
+            List<Edge> edgesToAdd = new ArrayList<>();
+            List<Edge> adjacents = adjacentEdges.getOrDefault(temp.getNode2(), new ArrayList<>());
+
+        //  Update probability value of each adjacent edge
+            for (Edge edge : adjacents) {
+                int node1 = edge.getNode1();
+                int node2 = edge.getNode2();
+                if (s[node2] != 1 && d[node2] < d[temp.getNode2()] * edge.getWeight()) {
+                    d[node2] = d[temp.getNode2()] * edge.getWeight();
+                    edgesToAdd.add(new Edge(node1, node2, d[node2]));
+                }
+            }
+
+            queue.addAll(edgesToAdd); // Add the new edges to the queue
+
+
+        }
+        return 0.0;
+    }
+
+    //  Static class to store edge data along with their weight
+    private static class Edge{
+
+        private final int node1;
+        private final int node2;
+        private final double weight;
+
+        //  Constructor
+        public Edge(int node1, int node2, double weight){
+            this.node1 = node1;
+            this.node2 = node2;
+            this.weight = weight;
+        }
+
+        //  Basic functions
+        public int getNode1(){
+            return this.node1;
+        }
+
+        public int getNode2(){
+            return this.node2;
+        }
+
+        public double getWeight(){
+            return this.weight;
         }
     }
 }
