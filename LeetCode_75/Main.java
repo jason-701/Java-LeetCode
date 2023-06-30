13662f5198fd82c28e347391a457fe063596519e
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-06-30 17:33:21 +0800

    movd some stuff around

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,6 +1,6 @@
 public class Main{
     public static void main(String[] args) {
-        String[] grid = {"@aABb"};
+        String[] grid = {"@aAbBcC"};
         daily_challenge test = new daily_challenge();
         System.out.println(test.shortestPathAllKeys(grid));
     }
