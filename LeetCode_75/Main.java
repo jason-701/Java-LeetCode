a914332d1ad4b07da0460bd674b97a6dc69b0273
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-07-04 10:16:19 +0800

    small change

--- LeetCode_75/Main.java
+++ LeetCode_75/Main.java
@@ -1,9 +1,9 @@
 public class Main{
     public static void main(String[] args) {
         //int[][] cells = {{1,1},{1,2},{2,1},{2,2}};
-        int[][] requests = {{1,2},{1,2},{2,2},{0,2},{2,1},{1,1},{1,2}};
+        int[] nums = {2,2,3,2};
         july_challenge test = new july_challenge();
-        System.out.println(test.maximumRequests(3, requests));
+        System.out.println(test.singleNumber(nums));
         
     }
 }
