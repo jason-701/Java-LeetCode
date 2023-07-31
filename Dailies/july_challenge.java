48abd9380a6fd72039328a47e798a554edc3d01a
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-07-31 21:33:53 +0800

    min ascii delete sum for two strings

--- Dailies/july_challenge.java
+++ Dailies/july_challenge.java
@@ -1340,4 +1340,33 @@ public class july_challenge {
         }
         return dp[0][n-1] + 1;
     }
+
+    //  31 July 2023
+    //  Minimum ASCII delete sum for two strings
+    public int minimumDeleteSum(String s1, String s2) {
+        //  We're using the longest common subsequence algorithm, but instead of length, it uses the sum of ascii values
+        int asciiSum = 0;
+        for (int i = 0; i < s1.length(); i++) {
+            asciiSum += s1.charAt(i);
+        }
+        for (int i = 0; i < s2.length(); i++) {
+            asciiSum += s2.charAt(i);
+        }
+
+        int[][] dp = new int[s1.length()+1][s2.length()+1];
+        for (int i = 0; i < s1.length() + 1; i++) {
+            for (int j = 0; j < s2.length() + 1; j++) {
+                if (i == 0 || j == 0){
+                    dp[i][j] = 0;
+                }
+                else if (s1.charAt(i-1) == s2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + s1.charAt(i-1);
+                }
+                else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return asciiSum - 2 * dp[s1.length()][s2.length()];
+    }
 }
