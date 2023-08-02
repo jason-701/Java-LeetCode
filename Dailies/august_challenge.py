fb2469971775f59d06d483ecd4f8fd92d7b7020f
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-08-02 11:14:38 +0800

    permutations

--- Dailies/august_challenge.py
+++ Dailies/august_challenge.py
@@ -23,6 +23,30 @@ class Solution(object):
         self.combineBackTracking(n, k, 1, result, [])
         return result
     
+    #   2 August 2023
+    #   Permuations
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        result = []
+        self.permuteBacktracking(nums, result, [])
+        return result
+        
+    def permuteBacktracking(self, nums, result, temp):
+            
+            if len(temp)==len(nums):
+                result.append(temp[:])
+                return
+            
+            for i in range(len(nums)):
+                if nums[i] in temp:
+                    continue
+                temp.append(nums[i])
+                self.permuteBacktracking(nums, result, temp)
+                temp.pop()
+        
     
 test = Solution()
-print(test.combine(4,3))
+print(test.permute([1,2,3]))
