7d0f7dbf71162f31422605e5fc5ef3a9a9c4f374
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-12-11 21:22:52 +0800

    c++ syntax is weird

---
+++ cpp_practice/hello.cpp
@@ -0,0 +1,13 @@
+// hello.cpp
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int age;
+    cout << "What is your age? ";
+    cin >> age;
+    cout << "You are " << age << " years old\n";
+}
