9b91145a2cc12d5392d98a1ca177f96b1b1cd643
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-12-11 23:26:06 +0800

    interesting function syntax

--- cpp_practice/hello.cpp
+++ cpp_practice/hello.cpp
@@ -4,10 +4,33 @@
 #include <iostream>
 using namespace std;
 
+void calculateBMI(double height, double weight, double& BMI);
+
 int main(int argc, char *argv[])
 {
     int age;
     cout << "What is your age? ";
     cin >> age;
-    cout << "You are " << age << " years old\n";
+
+    double height;
+    cout << "What is your height? ";
+    cin >> height;
+
+    double weight;
+    cout << "What is your weight? ";
+    cin >> weight;
+
+    double BMI = 0;
+    calculateBMI(height,weight,BMI);
+    cout << "You're " << age << " years old and " << "your BMI is " << BMI << endl;
+
+    if (BMI > 22)
+    {
+        cout << "You're a fatass" << endl;
+    }
+}
+
+void calculateBMI(double height, double weight, double &BMI)
+{
+    BMI =  weight / (height/100) / (height/100);
 }
