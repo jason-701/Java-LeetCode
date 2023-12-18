681b4d2df01641faa476f0c22ef320aa3c46d106
jason-701 <101868817+jason-701@users.noreply.github.com>
2023-12-18 09:46:18 +0800

    can place flowers asdjksakdjsadsadjsad what is this LOL

--- LeetCode_75_cpp/array_string.cpp
+++ LeetCode_75_cpp/array_string.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <numeric>
 #include <vector>
+#include <cmath>
 using namespace std;
 
 class Solution {
 public:
 
-// 16 Dec 2023
+//  16 Dec 2023
     string mergeAlternately(string word1, string word2) {
         int len1 = word1.length();
         int len2 = word2.length();
@@ -82,13 +83,98 @@ public:
         }
         return res;
     }
+
+//  18 Dec 2023
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count = 0;
+        if (flowerbed.size()==1 && flowerbed[0] == 0){
+            return (n<=1) ? true : false;
+        }
+
+        if (n > ceil(flowerbed.size()/2.0))
+        {
+            return false;
+        }
+
+        for (size_t i = 0; i < flowerbed.size(); i++)
+        {
+            // current location is occupied
+            if (flowerbed[i]==1)
+            {
+                continue;
+            }
+
+            // the previous location is occupied
+            if (i!=0 && flowerbed[i-1]==1)
+            {
+                continue;
+            }
+
+            // the next location is occupied
+            if (i<flowerbed.size()-1 && flowerbed[i+1]==1)
+            {
+                continue;
+            }
+
+            // at location 0, and the next location is empty
+            if (i==0 && flowerbed[i+1]==0)
+            {
+                count++;
+                continue;
+            }
+
+            // at location 1, and the previous location is empty, this means we would have planted the flower at the previous location
+            if (i==1 && flowerbed[0]==0)
+            {
+                continue;
+            }
+
+            if (i>3 && flowerbed[i-2]==0 && flowerbed[i-3]==0 && flowerbed[i-4]==1)
+            {
+                count++;
+                continue;
+            }
+
+            if (i>4 && flowerbed[i-2]==0 && flowerbed[i-3]==0 && flowerbed[i-4]==0 && flowerbed[i-5]==0)
+            {
+                count++;
+                continue;
+            }
+
+            if (i==3 && flowerbed[i-2]==0 && flowerbed[i-3]==0)
+            {
+                count++;
+                continue;
+            }
+
+            if (i==2 && flowerbed[0]==0)
+            {
+                count++;
+                continue;
+            }
+
+            // 2 plots before is occupied and the previous plot isn't, this means you can plant the flower at the current plot
+            if (i>1 && flowerbed[i-2]==1)
+            {
+                count++;
+                continue;
+            }
+        }
+        if (count >= n)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 };
 
 int main(int argc, char *argv[]){
     Solution solution;
-    string str1 = "ABCABC";
-    string str2 = "ABC";
-    string res = solution.gcdOfStrings(str1,str2);
+    vector<int> flowerbed = {1,0,0,0,0,0,0,1};
+    int res = solution.canPlaceFlowers(flowerbed,2);
     cout << "result: " << res << endl;
     return 0;
 }
