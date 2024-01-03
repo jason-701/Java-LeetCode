6c0bc6a8b0a5176b33720a096a490c0cc4d971ac
jason-701 <101868817+jason-701@users.noreply.github.com>
2024-01-03 12:01:03 +0800

    dota2 senate

--- LeetCode_75_cpp/queue.cpp
+++ LeetCode_75_cpp/queue.cpp
@@ -2,6 +2,7 @@
 #include <queue>
 using namespace std;
 
+//  3 Jan 2024
 class RecentCounter {
 public:
     queue<int> counter;
@@ -24,6 +25,42 @@ public:
         }
     }
 };
+
+//  3 Jan 2024
+class Dota{
+public:
+    string predictPartyVictory(string senate) {
+        queue<int> rad;
+        queue<int> dir;
+        for (int i = 0; i < senate.length(); i++){
+            if (senate[i]=='R'){
+                rad.push(i);
+            }
+            else{
+                dir.push(i);
+            }
+        }
+        int curIndex = senate.length();
+        while (!rad.empty() && !dir.empty())
+        {
+            //  Whoever that comes first has the ability to vote of the other person. After voting, it can vote again in the next round, hence the increased index
+            if (rad.front() > dir.front())
+            {
+                dir.push(dir.front()+curIndex);
+                dir.pop();
+                rad.pop();
+            }
+            else{
+                rad.push(rad.front()+curIndex);
+                rad.pop();
+                dir.pop();
+            }
+            curIndex++;
+        }
+        
+        return rad.empty() ? "Dire" : "Radiant";
+    }
+}
 
 /**
  * Your RecentCounter object will be instantiated and called as such:
